5be88cdad186c83af0dceab6d1062caed61287e3
bkharpuse <bkharpuse@gmail.com>
2024-09-18 16:52:11 +0530

    changing the host

--- app.py
+++ app.py
@@ -175,4 +175,4 @@ def answer():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
