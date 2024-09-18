e34daaee9b0b136964ad1e5c055ccc69b143d7a0
Bhupendra Kharpuse <bkharpuse@gmail.com>
2024-09-18 09:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__, static_folder='static')
 CORS(app)  # Enable CORS
 
 load_dotenv()
-google_api_key = os.environ["GOOGLE_API_KEY"]
+google_api_key = {var.GOOGLE_API_KEY}
 genai.configure(api_key=google_api_key)
 
 # Create the model
