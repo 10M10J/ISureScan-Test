65c63a5a597705259f51779867439f34ed1206a5
bkharpuse <bkharpuse@gmail.com>
2024-09-18 18:02:14 +0530

    df

--- static/app/services/upload.service.js
+++ static/app/services/upload.service.js
@@ -1,23 +1,27 @@
+const config = {
+    apiUrl: 'https://isurescan-756660766241.us-central1.run.app/'
+};
+
 // upload.service.js
 angular.module('myApp').service('UploadService', function($http) {
     this.uploadFile = function(file, language) {
         var formData = new FormData();
         formData.append('file', file);
         formData.append('language', language);
-        return $http.post('http://127.0.0.1:8080/upload', formData, {
+        return $http.post(config.apiUrl+'upload', formData, {
             headers: { 'Content-Type': undefined }
         });
     };
 
     this.summarize = function(text, language) {
-        return $http.post('http://127.0.0.1:8080/summarize', {
+        return $http.post(config.apiUrl + 'summarize', {
             text: text,
             language: language
         });
     };
 
     this.askQuestion = function(text, question, language) {
-        return $http.post('http://127.0.0.1:8080/answer', {
+        return $http.post(config.apiUrl + 'answer', {
             text: text,
             question: question,
             language: language
